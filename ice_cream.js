8de2efe45ea01cc25f69422f597be2f62b113f4d
larry <larryw3i@163.com>
2020-03-14 20:58:13 +0800

    zZ

--- ice_cream.js
+++ ice_cream.js
@@ -1,6 +1,131 @@
 
-function ice_cream( examination_text )
+function ice_cream( exam_text , preview_target )
 {
+    exam_text = String( exam_text );
     
+    var subheading_reg = /^=\s+(\w+)/;
+    var headline_reg = /^==\s+(\w+)/;
+    var exam_tip_reg = /^#\s+/;
+    var exam_q_reg = /^##\s+/;
+    var exam_q_a_reg = /^###\s+/
+    var exam_selector_reg = /^([A|B|C|D]\s+\w+)/
+    var exam_t_f_reg = />\s+[T|t|F|f]\s*/
+
+    var preview_html = '';
+
+    var exam_tip_index = 1;
+    var exam_q_index = 1;
+    var exam_q_a_index = 1 ;
+    
+    exam_text.split('\n').forEach( ( value , index, array ) => {
+
+        if(subheading_reg.test( value )){
+            preview_html += 
+                `<div style='text-align:center'><h3>`+
+                value.match(subheading_reg)[1]+
+                '</h3></div>';
+        }
+        else if(headline_reg.test( value ))
+        {
+            preview_html += 
+                `<div style='text-align:center'><h3>`+
+                value.match(headline_reg)[1]+
+                '</h3></div>';
+
+        }
+        else if(exam_tip_reg.test( value ))
+        {
+            preview_html +=
+                `<h4>`+ exam_tip_index+ '. '+
+                value.split(exam_tip_reg)[1]+'</h4>';
+            exam_tip_index++;    
+        }
+        else if( exam_q_reg.test(value) )
+        {
+            preview_html+= 
+                `<h5 style='margin-left:15px'>`+exam_q_index+'. '
+                + value.split( exam_q_reg )[1]+`</h5>` +
+                (value.indexOf('__') > -1?
+                `<input type='text' style='margin-left:20px'/>`:'');
+            exam_q_index++;
+        }
+        else if( exam_selector_reg.test(value) )
+        {
+            preview_html +=
+            `
+            <label style='margin-left:20px'>
+                <input type='checkbox'/>
+                ${value}
+            </label><br/>
+            `;
+        }
+        else if( exam_q_a_reg.test( value ) )
+        {
+            preview_html+=
+            `<h5 style='margin-left:20px'>
+            (${exam_q_a_index}). ${value.split( exam_q_a_reg )[1]}</h5>`;
+            exam_q_a_index++;
+        }
+        else if ( value =='?' )
+        {
+            preview_html +=
+            `
+                <textarea style='width:80%; margin-left:20px'
+                rows='10'></textarea>
+            `;
+        }
+        else if( exam_t_f_reg.test( value )  )
+        {
+            preview_html+=
+            `
+            <label style='margin-left:20px;'>
+                <input type='checkbox'/>✅
+            </label>
+            `;
+        }
+        else if( /^#audio\s+/.test(value) )
+        {
+            var value_splits = value.split("'");
+            preview_html+=
+            `
+            <div style='margin-left:20px; width:100%'>
+                <audio src="${value_splits[1]}" controls="controls"
+                    style='width:${value_splits[3]?'50%':value_splits[3]}'
+                    alt='${value_splits[5] ? value_splits[5]:"audio" }'>
+                </audio>
+            </div>
+            `;
+        }
+        else if( /^#video\s+/.test(value) )
+        {
+            var value_splits = value.split("'");
+            preview_html+=
+            `
+            <div style='margin-left:20px; width:100%'>
+                <video src="${value_splits[1]}" controls="controls"
+                    style='width:${value_splits[3]?'50%':value_splits[3]}'
+                    alt='${value_splits[5] ? value_splits[5]:"video" }'>
+                </video>
+            </div>
+            `;
+        }
+        else if( /^#image\s+/.test(value) )
+        {
+            var value_splits = value.split("'");
+            preview_html+=
+            `
+            <div style='margin-left:20px; width:100%'>
+                <img src="${value_splits[1]}"
+                    style='width:${value_splits[3]?'50%':value_splits[3]}'
+                    alt='${value_splits[5] ? value_splits[5]:'image'}'/>
+            </div>
+            `;
+        }
+        
+    });
+    
+    document.querySelector( preview_target ).insertAdjacentHTML( 
+        'beforeend', preview_html
+    );
 }
 
--- test.js
+++ test.js
@@ -1,97 +1,214 @@
 
 (function(){
-    examination_text = 
-`AAAAAAAAAAAAAA
-BBBBBBBBBBB
-CCCcccCCCCC
-==
-# DDDDDDDDDDDDDDDDDDDDDDDD 
-<5>
-## EEEEEEEEEEEEEEEEEEEEEEEEE TF
+    exam_text_0 = 
+`= xxxxx学校
+== 期末测试卷
+= 计算机
+
+# 判断题 
+## xxxxxxxxx是对的
 > T
-## FFFFFFFFFFFFFFFFFFFFFFFF TF
-> F
-## GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG TF
-> T
-## HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH TF
+
+## xxxxxxxaaaaaaxx是对的 
 > F
 
-# IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-<5>
+## aasssssssxxxxxxxxx是对的 
+> T
+## xxxxxxx dd fdg dfslg fdgs dfkghsfhxx是对的 
+> F
 
-## JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ( )JJJJJJJJJJJJJJJJJJJJ
-#media  'url' 'width' 'alt'
-### KKKKKKKKKKKKK
-### LLLLLLLLLLLLLLL
-### MMMMMMMMMMMMMMMM
-### NNNNNNNNNNNNNNNNNNNNNN
+# 选择题
+
+## 选( )还是AAAAAA
+A KKKKKKKKKKKKK
+B LLLLLLLLLLLLLLL
+C MMMMMMMMMMMMMMMM
+D NNNNNNNNNNNNNNNNNNNNNN
 > A
 
-## OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO( )OOOOOOO
-#audio  'url' 'width' 'alt'
-### PPPPPPP
-### QQQQQQQQQQQQQQQQQQ
-### RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
-### SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+## 这一题不选A的话选什么( )
+#audio  './audio_0.avi' '80%' 'audio_0'
+A PPPPPPP
+B QQQQQQQQQQQQQQQQQQ
+C RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+D SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 > B
 
-# TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
-<6>
+# 填空题
 
 ## UUUUUUUUUUUUUUUUUUU____UUUUUUUUUUUUUUUUUUUu___UUU 
-> VBC NBV
+> _VBC _NBV
 
 ## VVVVVVV____VVVVVVVVVVVVVVVVVV__VVVVVVVVVVV
-> LLLNNN MMMM
+> _LLLNNN _MMMM
 
 ## WWWWWWWWWWWWWWWWWWWWWW______WWWWWWWWWWWWWW
-> LLSS
+> _LLSS
 
 # XXXXXXXXXXXXXXXXXXXXXXXX
 
-## YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
-#image  'url' 'width' 'alt'
+## YYYYYYYYYYYYYYYYYY     YYYYYYYYYYYYYYYYY
+#image              './image_0.png' '80%' 'image_0'
+
 
 ### KLKLKLKLKLKLKLKLKLKLKLKLKLKLKLKLKLKLKL ?
-<10>
+?
 
 > HSDKJF HAFKh akf fha lfhadsfkj abva mnf lf alk
 a fhjaksfha skgg das
 as jakfsad gg asldfag sbaf fasd
 a fadhfa sgf adsjk asdkfk basjvba
 
-### BVBVBBVBVBVBVBVBBVBVBVBVBVBVBVBVBVBVBV ?
-<10>
-#video  'url' 'width' 'alt'
+### BVBVBBVBVBVBVBV    BBVBVBVBVBVBVBVBVBVBVBV ?
+?
+#video     './video_0.mp4' '80%' 'video_0'
 
-> dhg skdghs kghsdg hslgahga g
+> 
+dhg skdghs kghsdg hslgahga g
 ahag bxvbfhag 
 a jalhf ag
+>
 
-## ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ ? 
-<10>
-> hdfkkgh sdgkl hsdffgklflsh fgf kghagn dvuwh erugbdv x v ,ah ,kah,d agha 
+## ZZZZZZZZZZZZZZZZZZZZ    ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ ?
+?
+> 
+hdfkkgh sdgkl hsdffgklflsh fgf kghagn dvuwh erugbdv x v ,ah ,kah,d agha 
  gahlghakghagbcx,vaa
  ah algha ga
-
+>
 `;
+    var exam_text = '';
 
-var exam_split_lines = String( examination_text ).split('\n');
-var subheading = exam_split_lines[0];
-var headline = exam_split_lines[1];
-var subheading_0 = exam_split_lines[2];
+    var subheading_reg = /^=\s+/;
+    var headline_reg = /^==\s+/;
+    var exam_tip_reg = /^#\s+/;
+    var exam_q_reg = /^##\s+/;
+    var exam_q_a_reg = /^###\s+/
+    var exam_selector_reg = /^([A|B|C|D]\s+\w+)/
+    var exam_t_f_reg = />\s+[T|t|F|f]\s*/
 
-var exam_type_split_reg = /\s+#\s+/
 
-var exam_types = String( examination_text ).split('==')[1]
-    .split( exam_type_split_reg );
-exam_types = exam_types.slice( 1, exam_types.length );
+    var preview_html = '';
 
-exam_types.forEach( (t_value, t_index, t_array)=>{
-    console.log( 't_value '+ t_index );
-    console.log( t_value );
-});
+    var exam_tip_index = 1;
+    var exam_q_index = 1;
+    var exam_q_a_index = 1 ;
+    document.querySelector('#exam_text_teatarea').addEventListener(
+        'keyup',
+        ()=>{ 
+            exam_text = document.querySelector('#exam_text_teatarea').value;
+            exam_text.split('\n').forEach( ( value , index, array ) => {
 
+                if(subheading_reg.test( value )){
+                    preview_html += 
+                        `<div style='text-align:center'><h3>`+
+                        value.split(subheading_reg)[1]+
+                        '</h3></div>';
+                }
+                else if(headline_reg.test( value ))
+                {
+                    preview_html += 
+                        `<div style='text-align:center'><h3>`+
+                        value.split(headline_reg)[1]+
+                        '</h3></div>';
+        
+                }
+                else if(exam_tip_reg.test( value ))
+                {
+                    preview_html +=
+                        `<h4>`+ exam_tip_index+ '. '+
+                        value.split(exam_tip_reg)[1]+'</h4>';
+                    exam_tip_index++;    
+                }
+                else if( exam_q_reg.test(value) )
+                {
+                    preview_html+= 
+                        `<h5 style='margin-left:15px'>`+exam_q_index+'. '
+                        + value.split( exam_q_reg )[1]+`</h5>` +
+                        (value.indexOf('__') > -1?
+                        `<input type='text' style='margin-left:20px'/>`:'');
+                    exam_q_index++;
+                }
+                else if( exam_selector_reg.test(value) )
+                {
+                    preview_html +=
+                    `
+                    <label style='margin-left:20px'>
+                        <input type='checkbox'/>
+                        ${value}
+                    </label><br/>
+                    `;
+                }
+                else if( exam_q_a_reg.test( value ) )
+                {
+                    preview_html+=
+                    `<h5 style='margin-left:20px'>
+                    (${exam_q_a_index}). ${value.split( exam_q_a_reg )[1]}</h5>`;
+                    exam_q_a_index++;
+                }
+                else if ( value =='?' )
+                {
+                    preview_html +=
+                    `
+                        <textarea style='width:80%; margin-left:20px'
+                        rows='10'></textarea>
+                    `;
+                }
+                else if( exam_t_f_reg.test( value )  )
+                {
+                    preview_html+=
+                    `
+                    <label style='margin-left:20px;'>
+                        <input type='checkbox'/>✅
+                    </label>
+                    `;
+                }
+                else if( /^#audio\s+/.test(value) )
+                {
+                    var value_splits = value.split("'");
+                    preview_html+=
+                    `
+                    <div style='margin-left:20px; width:100%'>
+                        <audio src="${value_splits[1]}" controls="controls"
+                            style='width:${value_splits[3]?'50%':value_splits[3]}'
+                            alt='${value_splits[5] ? value_splits[5]:"audio" }'>
+                        </audio>
+                    </div>
+                    `;
+                }
+                else if( /^#video\s+/.test(value) )
+                {
+                    var value_splits = value.split("'");
+                    preview_html+=
+                    `
+                    <div style='margin-left:20px; width:100%'>
+                        <video src="${value_splits[1]}" controls="controls"
+                            style='width:${value_splits[3]?'50%':value_splits[3]}'
+                            alt='${value_splits[5] ? value_splits[5]:"video" }'>
+                        </video>
+                    </div>
+                    `;
+                }
+                else if( /^#image\s+/.test(value) )
+                {
+                    var value_splits = value.split("'");
+                    preview_html+=
+                    `
+                    <div style='margin-left:20px; width:100%'>
+                        <img src="${value_splits[1]}"
+                            style='width:${value_splits[3]?'50%':value_splits[3]}'
+                            alt='${value_splits[5] ? value_splits[5]:'image'}'/>
+                    </div>
+                    `;
+                }
+                
+            });
+            
+            document.querySelector('#exam_preview').insertAdjacentHTML( 
+                'beforeend', preview_html
+            );
+     }, false);
+    
 
 
 }());
